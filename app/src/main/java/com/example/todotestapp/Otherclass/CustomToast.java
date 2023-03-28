532d21e67f5eef9edebd715617078b7131f940bd
Flash newton <berajapakse@gmail.com>
2023-03-28 10:16:55 +0530

    final version

---
+++ app/src/main/java/com/example/todotestapp/Otherclass/CustomToast.java
@@ -0,0 +1,10 @@
+package com.example.todotestapp.Otherclass;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+
+import com.example.todotestapp.R;
+import com.example.todotestapp.Register;
+
+public class CustomToast{
+}
---
+++ app/src/main/java/com/example/todotestapp/Otherclass/ToDoModel.java
@@ -0,0 +1,31 @@
+package com.example.todotestapp.Otherclass;
+
+public class ToDoModel {
+    private int id;
+    private String task;
+    private String user;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
